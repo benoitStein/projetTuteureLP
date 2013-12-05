1a71cdc4d2a5623f4d1f6da8fd9f7d497a5be3a5
projetlp2013 <projetlp2013@info-s2-mac2.iut-bm.univ-fcomte.fr>
2013-12-05 13:23:56 +0100

    Ajout map

--- ApllicationIUT/ItineraireViewController.m
+++ ApllicationIUT/ItineraireViewController.m
@@ -14,6 +14,8 @@
 
 @implementation ItineraireViewController
 
+@synthesize mapView;
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -27,6 +29,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    self.mapView.showsUserLocation = YES;
 }
 
 - (void)didReceiveMemoryWarning
